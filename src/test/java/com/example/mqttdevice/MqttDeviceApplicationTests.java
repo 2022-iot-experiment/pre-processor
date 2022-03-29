b2d7647ac1befed08bd4b78f50109b764593074c
Bomou He <paakmau@outlook.com>
2022-03-29 18:32:38 +0800

    Spring Boot 模板项目

---
+++ src/test/java/com/example/mqttdevice/MqttDeviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mqttdevice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MqttDeviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
